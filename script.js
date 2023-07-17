49d3479c680a9bf4e764b7a2158e848a7392e0c8
cedogithub <39746523+cedogithub@users.noreply.github.com>
2023-07-17 10:02:57 -0400

    Update weather data display in card elements

--- script.js
+++ script.js
@@ -2,71 +2,101 @@
 const weatherFormEl = document.querySelector('.weather-form');
 const locationInputEl = document.querySelector('.weather-form__input');
 const weatherDataContainerEl = document.querySelector('.weather-data');
+const defaultLocation = 'Montreal';
 
 // Add event listener to the weather form when it is submitted
 weatherFormEl.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent the default form submission and page refresh
+  event.preventDefault(); // Prevent the default form submission and page refresh
 
-    const enteredLocation = locationInputEl.value.trim(); // Get the trimmed value entered in the location input field
+  const enteredLocation = locationInputEl.value.trim(); // Get the trimmed value entered in the location input field
 
-    if (enteredLocation === '') {
-        // Display an error message if the entered location is empty
-        weatherDataContainerEl.textContent = 'Please enter a valid location';
-    } else {
-        // Fetch weather data for the entered location and display it
-        displayWeatherData(enteredLocation)
-            .catch(handleError); // Handle any errors that occur during the fetch request
-    }
+  if (enteredLocation === '') {
+    // Display an error message if the entered location is empty
+    weatherDataContainerEl.textContent = 'Please enter a valid location';
+  } else {
+    // Fetch weather data for the entered location and display it
+    displayWeatherData(enteredLocation).catch(handleError); // Handle any errors that occur during the fetch request
+  }
 });
 
 // Function to create a weather information div element
 const createWeatherDiv = (className, textContent) => {
-    const weatherDiv = document.createElement('div'); // Create a new div element
-    weatherDiv.classList.add(className); // Add the specified class name to the div
-    weatherDiv.textContent = textContent; // Set the text content of the div
-    return weatherDiv;
+  const weatherDiv = document.createElement('div'); // Create a new div element
+  weatherDiv.classList.add(className); // Add the specified class name to the div
+  weatherDiv.textContent = textContent; // Set the text content of the div
+  return weatherDiv;
 };
 
 // Function to fetch weather data from the API based on the given location and display it
 const displayWeatherData = (location) => {
-    const API_KEY = '3d1853a7fd9b499794571220230607';
+  const API_KEY = '3d1853a7fd9b499794571220230607';
 
-    return fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Unable to fetch weather data');
-            }
-            return response.json(); // Parse the response JSON and return the data
-        })
-        .then(data => {
-            console.log(data); // Log the location information to the console
+  return fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Unable to fetch weather data');
+      }
+      return response.json(); // Parse the response JSON and return the data
+    })
+    .then((data) => {
+      // Clear the previous content in the weather data container
+      weatherDataContainerEl.textContent = '';
 
-            // Clear the previous content in the weather data container
-            weatherDataContainerEl.textContent = '';
+      // Create the card div
+      const cardDiv = document.createElement('div');
+      cardDiv.classList.add('weather-card');
 
-            // Create and append the weather information elements
-            const cityDiv = createWeatherDiv('weather-data__city', data.location.name);
-            const temperatureDiv = createWeatherDiv('weather-data__temperature', `${data.current.temp_c}°C`);
-            const humidityDiv = createWeatherDiv('weather-data__humidity', `Humidity: ${data.current.humidity}%`);
-            const currentWeatherDiv = createWeatherDiv('weather-data__current-weather', data.current.condition.text);
-            const updatedAtDiv = createWeatherDiv('weather-data__updated-at', `Updated at: ${data.current.last_updated}`);
-            const windDiv = createWeatherDiv('weather-data__wind', `Wind: ${data.current.wind_kph} km/h ${data.current.wind_dir}`);
+      // Create the card body div
+      const cardBodyDiv = document.createElement('div');
+      cardBodyDiv.classList.add('weather-card__body');
 
-            // Append the weather information elements to the weather data container
-            weatherDataContainerEl.appendChild(cityDiv);
-            weatherDataContainerEl.appendChild(temperatureDiv);
-            weatherDataContainerEl.appendChild(humidityDiv);
-            weatherDataContainerEl.appendChild(currentWeatherDiv);
-            weatherDataContainerEl.appendChild(updatedAtDiv);
-            weatherDataContainerEl.appendChild(windDiv);
-        });
+      // Create and append the weather information elements to the card body div
+      const cityDiv = createWeatherDiv('weather-data__city', data.location.name);
+      const temperatureDiv = createWeatherDiv('weather-data__temperature', `${data.current.temp_c}°C`);
+      const humidityDiv = createWeatherDiv('weather-data__humidity');
+      const humidityIconElement = document.createElement('i');
+      humidityIconElement.classList.add('fas', 'fa-droplet');
+      humidityDiv.appendChild(humidityIconElement);
+      humidityDiv.textContent += ` ${data.current.humidity}%`;
+      const currentWeatherDiv = createWeatherDiv('weather-data__current-weather', data.current.condition.text);
+      const last_updated = data.current.last_updated;
+      const timeString = last_updated.split(' ')[1];
+      const updatedAtDiv = createWeatherDiv('weather-data__updated-at', `Updated at: ${timeString}`);
+      const windDiv = createWeatherDiv('weather-data__wind');
+      const windIconElement = document.createElement('i');
+      windIconElement.classList.add('fas', 'fa-wind');
+      windDiv.appendChild(windIconElement);
+      windDiv.textContent += ` ${data.current.wind_kph} km/h ${data.current.wind_dir}`;
+
+      // Create the weather icon element
+      const weatherIconUrl = data.current.condition.icon;
+      const weatherIconElement = document.createElement('img');
+      weatherIconElement.src = weatherIconUrl;
+      weatherIconElement.classList.add('weather-data__icon');
+
+      // Append the weather information elements to the card body div
+      cardBodyDiv.appendChild(cityDiv);
+      cardBodyDiv.appendChild(updatedAtDiv);
+      cardBodyDiv.appendChild(weatherIconElement); // Add the weather icon element
+      cardBodyDiv.appendChild(temperatureDiv);
+      cardBodyDiv.appendChild(currentWeatherDiv);
+      cardBodyDiv.appendChild(humidityDiv);
+      cardBodyDiv.appendChild(windDiv);
+
+      // Append the card body div to the card div
+      cardDiv.appendChild(cardBodyDiv);
+
+      // Append the card div to the weather data container
+      weatherDataContainerEl.appendChild(cardDiv);
+    });
 };
 
 // Function to handle errors that occur during the fetch request
 const handleError = (error) => {
-    console.error('Error:', error); // Log the error message to the console
+  console.error('Error:', error); // Log the error message to the console
 
-    // Display an error message in the weather data container
-    weatherDataContainerEl.textContent = 'Error occurred while fetching weather data';
+  // Display an error message in the weather data container
+  weatherDataContainerEl.textContent = 'Error occurred while fetching weather data';
 };
-    
+
+displayWeatherData(defaultLocation);
